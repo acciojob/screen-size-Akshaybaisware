17ad2abeb9ad3aa246731b846e726b357437a493
Akshaybaisware <130745994+Akshaybaisware@users.noreply.github.com>
2023-06-02 16:03:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+window.onload = function(){
+	let sizeInfo = document.getElementById("sizeInfo");
+	let width,height;
+
+	function updateSizeInfo()
+		{
+			width = window.innerwidth;
+			height = window.innerheight;
+			sizeInfo.textContent = "width: "+ width + " and Height: " + height
+		}
+	updateSizeInfo();
+	window.addEventListener("resize" , updateSizeInfo);
+		};
